e86e27a87aeaece304e268fb580ee8d97ecb42a3
Hariharan Duraisamy (MS/EMT5-ETAS-VOS) <Duraisamy.Hariharan@in.bosch.com>
2023-11-02 12:02:07 +0530

    feat: Add VIP ARC BSW SW pipeline
    Add VIP ARC BSW pipeline with new release flow in the MPL workflow
    
    Refs: ADO-17049

---
+++ src/com/refSolution/pipelinerdepot/pipelines/ArcBswPipeline.groovy
@@ -0,0 +1,54 @@
+package com.refSolution.pipelinerdepot.pipelines
+
+import com.refSolution.pipelinerdepot.pipelines.CommonPipeline
+import com.refSolution.pipelinerdepot.stages.CommonStages
+import com.refSolution.pipelinerdepot.stages.ArcBswStages
+
+
+class ArcBswPipeline extends CommonPipeline {
+    CommonStages commonStages
+    
+    Boolean skipPipeline = false
+
+    ArcBswPipeline(script, Map env, Map ioMap) {
+        super(script, [
+            // the input keys and their default values for the pipeline, can be
+            // overridden by user inputs from either MR message or Jenkins env
+            defaultInputs: '''
+                archive_patterns
+                build_dir_path
+                bsw_dir_path
+                bsw_pre_build_file_name
+                autosar_tool
+                autosar_tool_version
+                autosar_tool_env
+                project_variant
+            ''',
+            // the keys exposed to the user for modification
+            exposed: [
+                'archive_patterns',
+                'build_dir_path',
+                'bsw_dir_path',
+                'bsw_pre_build_file_name',
+                'autosar_tool',
+                'autosar_tool_version',
+                'autosar_tool_env',
+                'project_variant'
+            ],
+            // the keys for which pipeline should be parallelized
+            parallel: []
+        ] as Map, env, ioMap)
+
+        // Specify the node label expression
+        // Looks like we can't use && syntax due to input parser
+        nodeLabelExpr = "windows-lab-pc"
+
+        
+        commonStages = new CommonStages(script, env)
+    }
+
+    @Override
+    void getCustomStages(){
+        ArcBswStages customStages = new ArcBswStages(script, env)
+    }
+}
