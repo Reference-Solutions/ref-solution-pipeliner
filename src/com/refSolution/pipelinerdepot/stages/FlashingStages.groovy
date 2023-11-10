487d818abc675f2a97c44c8116bdf021057830ed
Bandapalli Sreenath Reddy (MS/EVS-SDS-PJ-CST) <SreenathReddy.Bandapalli@in.bosch.com>
2023-11-10 19:20:32 +0530

    feat: Updating stageVerifyT32 method in flashingstages
    refs: ADO-18180

--- src/com/refSolution/pipelinerdepot/stages/FlashingStages.groovy
+++ src/com/refSolution/pipelinerdepot/stages/FlashingStages.groovy
@@ -32,14 +32,15 @@ class FlashingStages {
         if (process) {
             powershell """
                 $process.Kill()
-                Write-Host "The app is not running!!"
+                echo "The app is not running!!"
                 Start-Sleep -Seconds 30
             """
         } else {
-            Write-Host "The app is not running."
+            echo "The app is not running."
         }
     }
 }
+
 
     def stageFlashing(Map env, Map stageInput = [:]){
         String arcbsw_binary_path = stageInput.arcbsw_binary_dir?.trim() ?: ''
