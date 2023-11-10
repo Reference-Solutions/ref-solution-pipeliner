d7200512c455151c29dfb9d3454752e8dc679feb
Bandapalli Sreenath Reddy (MS/EVS-SDS-PJ-CST) <SreenathReddy.Bandapalli@in.bosch.com>
2023-11-10 19:38:16 +0530

    feat: Updating stageVerifyT32 method in flashingstages
    refs: ADO-18180

--- src/com/refSolution/pipelinerdepot/stages/FlashingStages.groovy
+++ src/com/refSolution/pipelinerdepot/stages/FlashingStages.groovy
@@ -25,16 +25,16 @@ class FlashingStages {
     }
 
     def stageVerifyT32(Map env, Map stageInput = [:]){
-    def process = bat(script: 'powershell Get-Process -Name "t32marm"', returnStatus: true)
-    if (process == 0) {
-        bat(script: 'powershell $process = Get-Process -Name "t32marm"; $process.Kill(); echo "The app is not running!!"; Start-Sleep -Seconds 30')
-    } else {
-        echo "The app is not running."
+        def appPath = "C:\\toolbase\\_ldata\\lauterbach_debugger\\t32_2022_r1_alpha2fp_x64\\bin\\windows64\\t32marm.exe"
+        def isRunning = bat(script: "tasklist /FI \"IMAGENAME eq t32marm.exe\" 2>NUL | find /I /N \"t32marm.exe\" >NUL", returnStatus: true) == 0
+        if (isRunning) {
+            echo "App is running. Stopping ..."
+            bat "taskkill /F /IM t32marm.exe"
+            sleep 20
+            } else {
+                echo "App is not running!!"
+            }
     }
-}
-
-
-
 
     def stageFlashing(Map env, Map stageInput = [:]){
         String arcbsw_binary_path = stageInput.arcbsw_binary_dir?.trim() ?: ''
