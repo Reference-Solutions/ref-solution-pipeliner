47218b5a8169846ab252ae37546a973d9948b60f
Bandapalli Sreenath Reddy (MS/EVS-SDS-PJ-CST) <SreenathReddy.Bandapalli@in.bosch.com>
2023-11-13 12:45:59 +0530

    feat: Updating jenkins node label.
    refs: ADO-18180

--- src/com/refSolution/pipelinerdepot/pipelines/Flashing.groovy
+++ src/com/refSolution/pipelinerdepot/pipelines/Flashing.groovy
@@ -9,7 +9,7 @@ class Flashing extends BasePipeline {
     
     Boolean skipPipeline = false
 
-    Flashing(script, Map env, Map ioMap) {
+    Flashing(script, Map env, Map ioMap, String nodeLabelExprParam = "windows-lab-pc") {
         super(script, [
             // the input keys and their default values for the pipeline, can be
             // overridden by user inputs from either MR message or Jenkins env
@@ -31,6 +31,8 @@ class Flashing extends BasePipeline {
         // Specify the node label expression
         // Looks like we can't use && syntax due to input parser
         //nodeLabelExpr = "windows-lab-pc"
+        // Set the nodeLabelExpr using the parameter value
+        nodeLabelExpr = nodeLabelExprParam
 
         
         flashingStages = new FlashingStages(script, env)
