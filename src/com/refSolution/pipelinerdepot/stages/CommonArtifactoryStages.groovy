82df67d9af0b7f451fee069b1f395896e2a01a20
Hariharan Duraisamy (MS/EMT5-ETAS-VOS) <Duraisamy.Hariharan@in.bosch.com>
2024-01-05 11:55:21 +0530

    feat : Added Nexus Upload/Downlaod process
    Added Artifactory upload and download process with jfrog / nexus / conan
    
    ref : ADO-18648

--- src/com/refSolution/pipelinerdepot/stages/CommonArtifactoryStages.groovy
+++ src/com/refSolution/pipelinerdepot/stages/CommonArtifactoryStages.groovy
@@ -2,7 +2,9 @@
 package com.refSolution.pipelinerdepot.stages
 
 import com.bosch.pipeliner.LoggerDynamic
-import com.refSolution.pipelinerdepot.utils.Artifactory
+import com.refSolution.pipelinerdepot.utils.Jfrog
+import com.refSolution.pipelinerdepot.utils.Conan
+import com.refSolution.pipelinerdepot.utils.Nexus
 
 /**
 * Contains stages that can be reused across pipelines
@@ -12,7 +14,9 @@ class CommonArtifactoryStages {
     private def script
     private Map env
     private LoggerDynamic logger
-    private Artifactory artifactory
+    private Jfrog jfrog
+    private Conan conan
+    private Nexus nexus
 
     /**
      * Constructor
@@ -23,25 +27,78 @@ class CommonArtifactoryStages {
     CommonArtifactoryStages(script, Map env) {
         this.script = script
         this.env = env
-        this.artifactory = new Artifactory(script, env)
+        this.jfrog = new Jfrog(script, env)
+        this.conan = new Conan(script, env)
+        this.nexus = new Nexus(script, env)
         this.logger = new LoggerDynamic(script)
     }
 
     def stageArtifactoryDownload(Map env, Map stageInput = [:]){
         script.stage("Artifactory Downlaod") { 
-            logger.info('DOWNLOAD FROM ARTIFACTORY STAGE')
-            String pattern = stageInput.artifactory_pattern.trim()
-            String target = stageInput.artifactory_target.trim()
-            artifactory.downloadFromArtifactory(pattern, target)
+            String downloadType = stageInput.artifactory_download_type?.trim()
+            if (downloadType == "jfrog"){
+                logger.info('DOWNLOAD FROM ARTIFACTORY STAGE')
+                String pattern = stageInput.artifactory_pattern.trim()
+                String target = stageInput.artifactory_target.trim()
+                jfrog.downloadFromArtifactory(pattern, target)
+            }
+            else if (downloadType == "nexus"){
+                logger.info('DOWNLOAD FROM NEXUS ARTIFACTORY STAGE')
+                
+                String nexusUrl = stageInput.nexus_url?.trim() ?: "http://10.165.38.203:8081"
+                String groupId = stageInput.nexus_group_Id.trim()
+                String version = stageInput.build_version.trim()
+                String repository = stageInput.nexus_repository.trim()
+                String credentialsId = stageInput.nexus_credentials_id.trim()
+                String projectName = stageInput.nexus_project_name.trim()
+                String targetDirectory = stageInput.nexus_download_dir?.trim() ?: "target"
+                String fileNameRegex = stageInput.nexus_file_pattern.trim()
+                String packaging = stageInput.nexus_packaging?.trim() ?: "zip"
+
+                nexus.download(nexusUrl, repository, groupId, projectName, version, credentialsId, packaging, targetDirectory, fileNameRegex)
+            }
+            else{
+                logger.warn('Artifactory Download is Skipped since Download Type is not proper. It sould be jfrog/nexus')
+            }
+
         }
     }
 
     def stageArtifactoryUpload(Map env, Map stageInput = [:]){
-        script.stage("Artifactory Downlaod") { 
-            logger.info('DOWNLOAD FROM ARTIFACTORY STAGE')
-            String pattern = stageInput.artifactory_upload_pattern.trim()
-            String target = stageInput.artifactory_upload_target.trim()
-            artifactory.uploadToArtifactory(pattern, target)
+        script.stage("Artifactory Upload") { 
+            String uploadType = stageInput.artifactory_upload_type?.trim()
+            if (uploadType == "jfrog"){
+                logger.info('Upload To Jfrog Artifactpty')
+                String pattern = stageInput.artifactory_upload_pattern.trim()
+                String target = stageInput.artifactory_upload_target.trim()
+                jfrog.uploadToArtifactory(pattern, target)
+            }
+            else if (uploadType == "nexus"){
+                logger.info('Upload to Nexus Artifactory')
+                String nexusVersion = stageInput.nexus_tool_version?.trim() ?: "nexus3"
+                String protocol = stageInput.nexus_protocol?.trim() ?: "http"
+                String nexusUrl = stageInput.nexus_url?.trim() ?: "http://10.165.38.203:8081"
+                
+                String groupId = stageInput.nexus_group_id.trim()
+                String version = stageInput.build_version.trim()
+                String repository = stageInput.nexus_repository.trim()
+                String credentialsId = stageInput.nexus_credentials_id.trim()
+                String projectName = stageInput.nexus_project_name.trim()
+                String classifier = stageInput.nexus_classifier?.trim() ?: ""
+                String filePattern = stageInput.nexus_file_pattern.trim()
+                String packaging = stageInput.nexus_packaging?.trim() ?: "zip"
+                
+                nexus.upload(nexusVersion, protocol, nexusUrl, groupId, version, repository, credentialsId, projectName, classifier, filePattern, packaging)
+            }
+            else if (uploadType == "conan"){
+                logger.info('Artifactory upload using conan')
+                String remoteName = stageInput.conan_remote_name_to_upload.trim()
+                String packageRef = stageInput.conan_package_ref_to_upload.trim()
+                conan.upload(remoteName, packageRef)
+            }
+            else{
+                logger.warn('Artifactory upload is Skipped since Upload Type is not proper. It sould be jfrog/nexus/conan')
+            }
         }
     }
 }
