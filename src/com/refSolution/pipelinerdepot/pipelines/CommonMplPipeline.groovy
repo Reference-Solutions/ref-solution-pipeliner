07c1f72d91ccf76d22417e8fa8dee62a943a9098
kiran <kiran.shankarmandal@in.bosch.com>
2024-01-09 14:49:05 +0530

    feat: Made some changes in common mpl pipeline test file. Creating a repo with proper MPL functions.

--- src/com/refSolution/pipelinerdepot/pipelines/CommonMplPipeline.groovy
+++ src/com/refSolution/pipelinerdepot/pipelines/CommonMplPipeline.groovy
@@ -1,8 +1,9 @@
 package com.refSolution.pipelinerdepot.pipelines
 
 import com.bosch.pipeliner.BasePipeline
-import com.refSolution.pipelinerdepot.stages.CommonArtifactoryStages
-//import com.refSolution.pipelinerdepot.stages.CommonMplStages
+//import com.refSolution.pipelinerdepot.stages.CommonArtifactoryStages
+import com.refSolution.pipelinerdepot.stages.CommonMplStages
+import com.refSolution.pipelinerdepot.stages.CommonStages
 import com.refSolution.pipelinerdepot.stages.QnxStages
 
 
