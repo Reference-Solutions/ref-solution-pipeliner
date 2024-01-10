40c63d9b376e3b14e1f2c1bac7e9a83f41732089
wwt1cob <wwt1cob@bosch.com>
2024-01-10 12:49:40 +0530

    pantaris code

--- src/com/refSolution/pipelinerdepot/stages/CommonRestApiStages.groovy
+++ src/com/refSolution/pipelinerdepot/stages/CommonRestApiStages.groovy
@@ -38,6 +38,7 @@ class CommonRestApiStages {
         script.stage("getDesirtedStateByName"){
             logger.info("getDesirtedStateByName")
             String tToken = stageInput.accessToken
+            logger.info(tToken)
             def command = """curl -X GET -H "Authorization: Bearer $tToken" https://ota.eu.bosch-mobility-cloud.com/api/applications/ota/desiredStates/Test2__AVH_app_test_install"""
             return script.sh(script: command, returnStdout: true).trim()
            
