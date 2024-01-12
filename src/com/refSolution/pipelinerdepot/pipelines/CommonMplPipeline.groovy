a963182483c98d5bf78f3201d18a97eeaf8a79ec
kiran <kiran.shankarmandal@in.bosch.com>
2024-01-12 09:57:44 +0530

    feat: Made some changes in commonmpl pipeline file. Creating a repo with proper MPL functions.

--- src/com/refSolution/pipelinerdepot/pipelines/CommonMplPipeline.groovy
+++ src/com/refSolution/pipelinerdepot/pipelines/CommonMplPipeline.groovy
@@ -1,97 +1,46 @@
 package com.refSolution.pipelinerdepot.pipelines
 
-import com.bosch.pipeliner.BasePipeline
-import com.refSolution.pipelinerdepot.stages.CommonArtifactoryStages
-import com.refSolution.pipelinerdepot.stages.CommonMplStages
-import com.refSolution.pipelinerdepot.stages.QnxStages
+import com.refSolution.pipelinerdepot.pipelines.CommonPipeline
 import com.refSolution.pipelinerdepot.stages.CommonStages
-import com.refSolution.pipelinerdepot.pipelines.QnxPipeline
+import com.refSolution.pipelinerdepot.stages.CommonMplStages
 
 
+class CommonMplPipeline extends CommonPipeline {
+    CommonStages commonStages
+    
+    Boolean skipPipeline = false
 
-
-
-class CommonMplPipeline extends BasePipeline {
-      CommonArtifactoryStages commonArtifactoryStages
-      CommonStages commonStages
-
- void execQnxPipeline(Map stageInput) {
-        // Move the logic of execQnxPipeline here
-        // You can use the 'script' object if needed
-        // ...
-
-        // Example:
-        script.echo "Executing QnxPipeline with stageInput: ${stageInput}"
-    }     
-      
-      
-
-      Boolean skipPipeline = false
-
-    CommonMplPipeline(script, Map defaults, Map env, Map ioMap) {
+    CommonMplPipeline(script, Map env, Map ioMap) {
         super(script, [
             // the input keys and their default values for the pipeline, can be
             // overridden by user inputs from either MR message or Jenkins env
-            defaultInputs: """
-               
-               """
-       + defaults.defaultInputs,
+            defaultInputs: '''
+                archive_patterns = qnx-hv-nxp-s32g/images/*.ui
+                custom_scm_checkout_dir = qnx-hv-nxp-s32g
+                qnx_sdk_path = C:/Users/zrd2kor/qnx710
+            ''',
             // the keys exposed to the user for modification
-            exposed:[
-
-             ] + defaults.exposed,
-             // the keys for which pipeline should be parallelized
-            parallel: [] + defaults.parallel
+            exposed: [
+                'archive_patterns',
+                'custom_scm_checkout_dir',
+                'qnx_sdk_path',
+              	'pfe_copy'
+            ],
+            // the keys for which pipeline should be parallelized
+            parallel: []
         ] as Map, env, ioMap)
 
-        commonArtifactoryStages = new CommonArtifactoryStages(script, env) 
+        // Specify the node label expression
+        // Looks like we can't use && syntax due to input parser
+        nodeLabelExpr = "windows-lab-pc"
+
+        
         commonStages = new CommonStages(script, env)
-        
-
     }
 
-     // /**
-    // * Provides implementation for stages
-    // *
-    // * @param A Map with the inputs for stages
-    // */
     @Override
-    void stages(Map stageInput) {
-        
-        logger.info("customStages")
-        def customStages = getCustomStages()
-        // Skip the entire pipeline if we promote and there are no changes
-        if (skipPipeline) {
-            return
-        }
-        logger.info("stageInput")
-        logger.info(stageInput.inspect())
-        script.stage("VRTE pull") 
-        script.stage("QNX+BSW build and relaese")
-        // Call execQnxPipeline script
-        execQnxPipeline(stageInput)
-        script.stage("OPD/AVH Applications")
-        script.stage("QEMU Validation")
-        script.stage("Flashing M7 Software Application")
-        script.stage("Validation of HiL")
-        script.stage("Create S/W Package Artifacts")
-        script.stage("Create Vehicle Package Artifacts")
-
-        
-
-        
-
-
-    }
-
-
     void getCustomStages(){
         CommonMplStages customStages = new CommonMplStages(script, env)
-
-        
     }
-     
-     
-
 }
 
--- src/com/refSolution/pipelinerdepot/stages/CommonMplStages.groovy
+++ src/com/refSolution/pipelinerdepot/stages/CommonMplStages.groovy
@@ -1,26 +1,20 @@
-
 package com.refSolution.pipelinerdepot.stages
 
 import com.bosch.pipeliner.LoggerDynamic
 import com.bosch.pipeliner.ScriptUtils
+import com.refSolution.pipelinerdepot.stages.CommonStages
+
 
 /**
 * Contains stages that can be reused across pipelines
 */
 class CommonMplStages {
 
-    /**
-     * for shallow clone using gitlab_branch_source plugin, the minimum depth
-     * has to be at least the number of commits in the MR + 1, so the latest
-     * commit from main branch is also fetched, and the MR can be merged into it.
-     * in case the value is not sets, perform a full clone
-     */
-    final int defaultShallowCloneDepth = -1
-
     private def script
     private Map env
     private LoggerDynamic logger
     private ScriptUtils utils
+    private CommonStages commonStages
 
     /**
      * Constructor
@@ -31,20 +25,43 @@ class CommonMplStages {
     CommonMplStages(script, Map env) {
         this.script = script
         this.env = env
-        this.utils = new ScriptUtils(script, env)
         this.logger = new LoggerDynamic(script)
+        this.utils = new ScriptUtils(script, env)
+        this.commonStages = new CommonStages(script, env)
     }
     
-     
-        def stageBuild(Map env, Map stageInput = [:]){
+    def stageBuild(Map env, Map stageInput = [:]){
+      	
+      	String pfeCopy = stageInput.pfe_copy?.trim() ?: 'false'
+        if(pfeCopy == "true"){
+            script.stage("Copy PFE") {  
+              copyPFE(env, stageInput)
+            }
+        }
         script.stage("Build") {
-             logger.info("Build")
- 
+            makeBuild(env, stageInput)
+        }
     }
 
-}
+    def makeBuild(Map env, Map stageInput = [:]){
+        String qnxSdkPath = stageInput.qnx_sdk_path?.trim() ?: 'C:/Users/zrd2kor/qnx710'
+        String scm_checkout_dir = stageInput.custom_scm_checkout_dir?.trim() ?: ''
+        script.bat """
+            echo 'Set QNX env variable'
+            call ${qnxSdkPath}/qnxsdp-env.bat
+            echo 'starting building'
+            cd ${scm_checkout_dir}
+            make all
+        """
+    }
 
-
+    def copyPFE(Map env, Map stageInput = [:]){
+        String scm_checkout_dir = stageInput.custom_scm_checkout_dir?.trim() ?: 'C:/Users/zrd2kor/qnx710'
+        script.powershell """
+            cd ${scm_checkout_dir}
+            Copy-Item -Path 'pfe_1_1_0/*' -Destination 'pfe/' -Recurse -force
+        """
+    }
 }
 
 
