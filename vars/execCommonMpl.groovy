de37d229ecde18acde9b8728ebd8720032d85222
kiran <kiran.shankarmandal@in.bosch.com>
2024-01-12 10:01:25 +0530

    feat: Made some changes in commonmpl pipeline file. Creating a repo with proper MPL functions.

--- vars/execCommonMpl.groovy
+++ vars/execCommonMpl.groovy
@@ -1,23 +1,17 @@
 import com.refSolution.pipelinerdepot.pipelines.CommonMplPipeline
 
-
 import com.bosch.pipeliner.*
 
 /**
- * execCommonPipeline
- * The script is used to run CommonPipeline.
- * It can be invoked from a Jenkinsfile by writing 'execCommonPipeline()'
+ * execQnxPipeline
+ * The script is used to run QnxPipeline.
+ * It can be invoked from a Jenkinsfile by writing 'execQnxPipeline()'
  *
 
  */
 def call(Map stageOverriders=[:]) {
     Map ioMap = [:]
     Map environment = env.getEnvironment()
-    Map defaults = [
-        defaultInputs:'',
-        exposed: [],
-        parallel: []
-    ]
 
     // Override variables from Jenkinsfile with parameter values defined in the job
     if (params && !(env.PIP_NO_PARAMS || env.NO_PARAMS)) {
@@ -27,7 +21,6 @@ def call(Map stageOverriders=[:]) {
     }
 
     // Run the pipeline
-    CommonMplPipeline pipeline = new CommonMplPipeline(this, defaults, environment, ioMap)
+    CommonMplPipeline pipeline = new CommonMplPipeline(this, environment, ioMap)
     ioMap = pipeline.run()
-    
 }
