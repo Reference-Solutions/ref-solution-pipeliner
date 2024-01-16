b023276fec5fa2f9d5e6f67555154b6c8d41de37
kiran <kiran.shankarmandal@in.bosch.com>
2024-01-16 12:28:54 +0530

    add in commonmpl file

--- src/com/refSolution/pipelinerdepot/stages/CommonMplStages.groovy
+++ src/com/refSolution/pipelinerdepot/stages/CommonMplStages.groovy
@@ -34,39 +34,11 @@ class CommonMplStages {
         // add vrte pull stages here
         script.echo "Vrte pull"
     }
+
+    def QnxBuild(Map env, Map stageInput = [:]) {
+        // add OPD/AVH Applications stages here
+        script.echo "QNX stages add"
     
-    def stageBuild(Map env, Map stageInput = [:]){
-      	
-      	String pfeCopy = stageInput.pfe_copy?.trim() ?: 'false'
-        if(pfeCopy == "true"){
-            script.stage("Copy PFE") {  
-              copyPFE(env, stageInput)
-            }
-        }
-        script.stage("Build") {
-            makeBuild(env, stageInput)
-        }
-    }
-
-    def makeBuild(Map env, Map stageInput = [:]){
-        String qnxSdkPath = stageInput.qnx_sdk_path?.trim() ?: 'C:/Users/zrd2kor/qnx710'
-        String scm_checkout_dir = stageInput.custom_scm_checkout_dir?.trim() ?: ''
-        script.bat """
-            echo 'Set QNX env variable'
-            call ${qnxSdkPath}/qnxsdp-env.bat
-            echo 'starting building'
-            cd ${scm_checkout_dir}
-            make all
-        """
-    }
-
-    def copyPFE(Map env, Map stageInput = [:]){
-        String scm_checkout_dir = stageInput.custom_scm_checkout_dir?.trim() ?: 'C:/Users/zrd2kor/qnx710'
-        script.powershell """
-            cd ${scm_checkout_dir}
-            Copy-Item -Path 'pfe_1_1_0/*' -Destination 'pfe/' -Recurse -force
-        """
-    }
     
     def OPDAVHApplications(Map env, Map stageInput = [:]) {
         // add OPD/AVH Applications stages here
