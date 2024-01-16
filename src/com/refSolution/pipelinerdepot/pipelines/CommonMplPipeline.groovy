082c99fe416d9d0ab9d8f5447a42d4e808a3aa5d
kiran <kiran.shankarmandal@in.bosch.com>
2024-01-16 14:00:36 +0530

    add in commonmpl file

--- src/com/refSolution/pipelinerdepot/pipelines/CommonMplPipeline.groovy
+++ src/com/refSolution/pipelinerdepot/pipelines/CommonMplPipeline.groovy
@@ -15,17 +15,12 @@ class CommonMplPipeline extends CommonPipeline {
             // the input keys and their default values for the pipeline, can be
             // overridden by user inputs from either MR message or Jenkins env
             defaultInputs: '''
-                archive_patterns = qnx-hv-nxp-s32g/images/*.ui
-                custom_scm_checkout_dir = qnx-hv-nxp-s32g
-                qnx_sdk_path = C:/Users/zrd2kor/qnx710
+                
                 
             ''',
             // the keys exposed to the user for modification
             exposed: [
-                'archive_patterns',
-                'custom_scm_checkout_dir',
-                'qnx_sdk_path',
-              	'pfe_copy'
+              
                 
             ],
             // the keys for which pipeline should be parallelized
