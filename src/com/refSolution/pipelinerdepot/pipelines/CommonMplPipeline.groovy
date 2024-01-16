9d6285f9015411a11948cd336e04ee1e0030c344
kiran <kiran.shankarmandal@in.bosch.com>
2024-01-16 14:18:12 +0530

    add in commonmpl file

--- src/com/refSolution/pipelinerdepot/pipelines/CommonMplPipeline.groovy
+++ src/com/refSolution/pipelinerdepot/pipelines/CommonMplPipeline.groovy
@@ -1,11 +1,10 @@
 package com.refSolution.pipelinerdepot.pipelines
-import com.bosch.pipeliner.BasePipeline
-
+import com.refSolution.pipelinerdepot.pipelines.CommonPipeline
 
 import com.refSolution.pipelinerdepot.stages.CommonMplStages
 
 
-class CommonMplPipeline extends BasePipelin {
+class CommonMplPipeline extends CommonPipeline {
  
     
     Boolean skipPipeline = false
