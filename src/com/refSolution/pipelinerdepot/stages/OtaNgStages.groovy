8af21e12122c10032c9e79f0875e185d2a2545e8
Pooja Bhandari (MS/EVS-SDS-PJ-CST) <Pooja.JitendraBhandari@in.bosch.com>
2024-01-19 18:11:31 +0530

    Added new method to pull releases from Github

--- src/com/refSolution/pipelinerdepot/stages/OtaNgStages.groovy
+++ src/com/refSolution/pipelinerdepot/stages/OtaNgStages.groovy
@@ -41,7 +41,7 @@ class OtaNgStages {
                 String repo = stageInput.Github_repo?.trim()
                 String pattern = stageInput.Github_pattern?.trim()
 
-                GhCli.GhCliReleaseDownload(String releaseTag, String owner, String repo, String pattern)
+                GhCli.GhCliReleaseDownload(releaseTag, owner, repo, pattern)
         }
     }
     def stageSwPackgeCreation(Map env, Map stageInput = [:]){
