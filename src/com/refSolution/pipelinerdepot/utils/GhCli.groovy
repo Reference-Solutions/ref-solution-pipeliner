34e85c390976f4c6716fce7dbb6f7cae3e64822d
Pooja Bhandari (MS/EVS-SDS-PJ-CST) <Pooja.JitendraBhandari@in.bosch.com>
2024-01-21 21:53:11 +0530

    Added new method to pull releases from Github

--- src/com/refSolution/pipelinerdepot/utils/GhCli.groovy
+++ src/com/refSolution/pipelinerdepot/utils/GhCli.groovy
@@ -31,7 +31,7 @@ public class GhCli {
         
     }
 
-    def  GhCliReleaseDownload(String releasetag, String owner, String repo, String pattern){
+    def  GhCliReleaseDownload(def releasetag, def owner, def repo, def pattern){
 // gh auth login
 // gh auth logout
                 script.sh """
