1b6759f09ea7819317afd2f777d3126c19c9555f
Pooja Bhandari (MS/EVS-SDS-PJ-CST) <Pooja.JitendraBhandari@in.bosch.com>
2024-01-21 18:07:17 +0530

    Added new method to pull releases from Github

--- src/com/refSolution/pipelinerdepot/pipelines/OtaNgPipeline.groovy
+++ src/com/refSolution/pipelinerdepot/pipelines/OtaNgPipeline.groovy
@@ -59,11 +59,10 @@ class OtaNgPipeline extends BasePipeline {
         }
         logger.info("stageInput")
         logger.info(stageInput.inspect())
-        customStages.stageDownloadApplication(env, stageInput)
 
         if (stageInput.checkout_scm_stage == "true")
             commonGitStages.stageCheckoutSCM(env, stageInput)
-        
+        customStages.stageDownloadApplication(env, stageInput)
         customStages.stageSwPackgeCreation(env, stageInput)
         customStages.stageVehicePackgeCreation(env, stageInput)
         
