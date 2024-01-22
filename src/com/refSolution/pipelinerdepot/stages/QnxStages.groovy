67e19318967be022907e3cea691c7e22878fad3f
kiran <kiran.shankarmandal@in.bosch.com>
2024-01-22 12:14:39 +0530

    add in CommonMpl

--- src/com/refSolution/pipelinerdepot/stages/QnxStages.groovy
+++ src/com/refSolution/pipelinerdepot/stages/QnxStages.groovy
@@ -5,7 +5,6 @@ import com.bosch.pipeliner.ScriptUtils
 import com.refSolution.pipelinerdepot.stages.CommonStages
 
 
-
 /**
 * Contains stages that can be reused across pipelines
 */
@@ -16,7 +15,6 @@ class QnxStages {
     private LoggerDynamic logger
     private ScriptUtils utils
     private CommonStages commonStages
-    
 
     /**
      * Constructor
@@ -30,38 +28,36 @@ class QnxStages {
         this.logger = new LoggerDynamic(script)
         this.utils = new ScriptUtils(script, env)
         this.commonStages = new CommonStages(script, env)
-        
     }
     
     def stageBuild(Map env, Map stageInput = [:]){
       	
+        String qnxSdkPath = stageInput.qnx_sdk_path.trim()
+        String qnxSrcDir = stageInput.qnx_src_dir.trim()
       	String pfeCopy = stageInput.pfe_copy?.trim() ?: 'false'
         if(pfeCopy == "true"){
-            script.stage("Copy PFE") {  
-              copyPFE(env, stageInput)
+            script.stage("QNX Copy PFE") {
+                copyPFE(qnxSrcDir)
             }
         }
-        script.stage("Build") {
-            makeBuild(env, stageInput)
+        script.stage("QNX Build") {
+            makeBuild(qnxSdkPath, qnxSrcDir)
         }
     }
 
-    def makeBuild(Map env, Map stageInput = [:]){
-        String qnxSdkPath = stageInput.qnx_sdk_path?.trim() ?: 'C:/Users/zrd2kor/qnx710'
-        String scm_checkout_dir = stageInput.custom_scm_checkout_dir?.trim() ?: ''
+    def makeBuild(def qnxSdkPath, def qnxSrcDir){
         script.bat """
             echo 'Set QNX env variable'
             call ${qnxSdkPath}/qnxsdp-env.bat
             echo 'starting building'
-            cd ${scm_checkout_dir}
+            cd ${qnxSrcDir}
             make all
         """
     }
 
-    def copyPFE(Map env, Map stageInput = [:]){
-        String scm_checkout_dir = stageInput.custom_scm_checkout_dir?.trim() ?: 'C:/Users/zrd2kor/qnx710'
+    def copyPFE(def qnxSrcDir){
         script.powershell """
-            cd ${scm_checkout_dir}
+            cd ${qnxSrcDir}
             Copy-Item -Path 'pfe_1_1_0/*' -Destination 'pfe/' -Recurse -force
         """
     }
