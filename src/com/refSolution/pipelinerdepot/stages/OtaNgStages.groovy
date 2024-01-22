96c0b5c2bbf904361cb9800ae839b0aad2791559
Hariharan Duraisamy (MS/EMT5-ETAS-VOS) <Duraisamy.Hariharan@in.bosch.com>
2024-01-22 10:15:12 +0530

    feat: sw factory pipeline creation
    sw factory pipeline creation

--- src/com/refSolution/pipelinerdepot/stages/OtaNgStages.groovy
+++ src/com/refSolution/pipelinerdepot/stages/OtaNgStages.groovy
@@ -18,6 +18,7 @@ class OtaNgStages {
     private def appName
     private def appVersion
     private def actionType
+    private def packageName
 
     /**
      * Constructor
@@ -38,16 +39,17 @@ class OtaNgStages {
             String swcDirPath = stageInput.swc_dir_path?.trim() ?: 'ota-ng/swpkg/install_swc/'
             String genSwpDirPath = stageInput.gen_swp_dir_path?.trim() ?: 'ota-ng/swpkg/gen_swp_fb/'
             String appDirPath = stageInput.app_dir_path.trim()
-            String vrtefsToolPath = stageInput.vrtefs_tool_path?.trim() ?: 'C:/toolbase/vrte_adaptive_studio/r23-10/vrte_fs/'
+            String vrtefsToolPath = stageInput.vrtefs_tool_path?.trim() ?: 'C:/toolbase/vrte_adaptive_studio/r23-08/vrte_fs/'
             
             appName = stageInput.app_name.trim()
             appVersion = stageInput.app_version?.trim() ?: '1.0.0'
             actionType =  stageInput.action_type?.trim() ?: 'INSTALL'
-
-            updateArxmlWithAppDetails(swcDirPath + "software_cluster.arxml")
-            updateArxmlWithAppDetails(swcDirPath + "software_package.arxml")
+            packageName = "${actionType.toLowerCase()}_swc_app".toLowerCase()
+            
+            //updateArxmlWithAppDetails(swcDirPath + "software_cluster.arxml")
+            //updateArxmlWithAppDetails(swcDirPath + "software_package.arxml")
             generateFlatbufferConfigFiles(swcDirPath, vrtefsToolPath.trim())
-            removeKeyFromFlatCfgJson(swcDirPath + "gen/${appName}/upd__${appName}_flatcfg.json")
+            removeKeyFromFlatCfgJson(swcDirPath + "gen/${packageName}/upd__${packageName}_flatcfg.json")
             copyfilestoGenerateSWPackageFolder(swcDirPath, appDirPath, genSwpDirPath )
             createSWPackageBinary(genSwpDirPath)
         }
@@ -66,7 +68,7 @@ class OtaNgStages {
             def fileContent = script.readFile(file: filePath).trim()
             
             // Replace all occurrences of the search string
-            fileContent = fileContent.replaceAll("__APP_NAME__", appName)
+            fileContent = fileContent.replaceAll("__APP_PACKAGE_NAME__", packageName)
             fileContent = fileContent.replaceAll("__APP_VERSION__", appVersion)
             fileContent = fileContent.replaceAll("__ACTION_TYPE__", actionType)
             
@@ -89,7 +91,28 @@ class OtaNgStages {
         
         logger.info("Flatbuffer config files Generation Completed...")
     }
+    def removeKeyFromFlatCfgFile(def filePath){
+        
+        def keyToRemove = 'keyValueStorage'
+        if (script.fileExists(filePath)) {
+            // Read the file content
+            def fileContent = script.readFile(file: filePath).trim()
+            
+            // Replace all occurrences of the search string
+            //fileContent = fileContent.replaceAll(',.*PersistencyKeyValueStorage[0]"', packageName)
+            
+            //  Remove the key-value pair using replaceAll
+            fileContent = fileContent.replaceAll(",\\s*\"${keyToRemove}\":\\s*\"[^\"]*\"")
+            
+            // Write the modified content back to the file
+            script.writeFile(file: filePath, text: fileContent)
 
+            logger.info("Updated Application Details in SW cluster and package Arxml files.")
+        }
+        else{
+            logger.error("file : $filePath not exists")
+        }
+    }
     def removeKeyFromFlatCfgJson(def jsonFilePath){        
         
         def keyToRemove = 'keyValueStorage'
@@ -105,7 +128,7 @@ class OtaNgStages {
 
     def copyfilestoGenerateSWPackageFolder(def swcDirPath, def appDirPath, def genSwpDirPath){
         script.sh """
-            cp ${swcDirPath}gen/${appName}/upd__${appName}_flatcfg.json ${genSwpDirPath}
+            cp ${swcDirPath}gen/${packageName}/upd__${packageName}_flatcfg.json ${genSwpDirPath}
             cp ${swcDirPath}gen/upd_flatcfg.fbs ${genSwpDirPath}
             cp ${appDirPath}${appName} ${genSwpDirPath}
         """
@@ -114,15 +137,16 @@ class OtaNgStages {
     def createSWPackageBinary(def genSwpDirPath){
         script.stash includes: "${genSwpDirPath}**", name: 'gen_swp_fb'
         script.node("ubuntu-lab-pc"){
+            script.cleanWs()
             script.unstash "gen_swp_fb"
             script.sh """
                 cd ${genSwpDirPath}
-                python gen_swp_fb.py --container-format 0x0106 --update-manifest-data upd__${appName}_flatcfg.json --update-manifest-schema upd_flatcfg.fbs --artefact ${appName} compressWhole --compress zlib --key-store keys/ --block-size 5000 --verification On --flatc-path flatc --estimated-speed 1024
-                tar -cvf app_${appName}_${actionType}_v${appVersion}.swpkg swpkg.bin
+                tar -r --numeric-owner --owner=0 --group=0 -f ${packageName}_1_random_data.tar ${appName}
+                python3 gen_swp_fb.py --container-format 0x0106 --update-manifest-schema upd_flatcfg.fbs --update-manifest-data upd__${packageName}_flatcfg.json --artefact ${packageName}_1_random_data.tar compressChunks --key-store keys/ --compress None --output ${packageName}_${appName}.swpkg --flatc-path flatc --estimated-speed 15360 --block-size 64535
             """
-            swPackageSize = script.sh(script: "stat -c %s ${genSwpDirPath}app_${appName}_${actionType}_v${appVersion}.swpkg", returnStdout: true).trim()
-            script.stash includes: "${genSwpDirPath}app_${appName}_${actionType}_v${appVersion}.swpkg", name: 'swpkg'
-            script.archiveArtifacts "${genSwpDirPath}app_${appName}_${actionType}_v${appVersion}.swpkg"
+            swPackageSize = script.sh(script: "stat -c %s ${genSwpDirPath}${packageName}_${appName}.swpkg", returnStdout: true).trim()
+            script.stash includes: "${genSwpDirPath}${packageName}_${appName}.swpkg", name: 'swpkg'
+            script.archiveArtifacts "${genSwpDirPath}${packageName}_${appName}.swpkg"
         }
         script.unstash "swpkg"
     }
@@ -134,9 +158,9 @@ class OtaNgStages {
             cd ${vpkgDirPath}
             tar -cvf signedcontainer.tar vehicle_package_manifest.json
             touch signature.crt
-            tar -cvf vehiclepkg_app_${appName}_${actionType}_v${appVersion}.tar signedcontainer.tar signature.crt
+            tar -cvf vehiclepkg_${packageName}_${appName}.tar signedcontainer.tar signature.crt
         """
-        script.archiveArtifacts "${vpkgDirPath}vehiclepkg_app_${appName}_${actionType}_v${appVersion}.tar"
+        script.archiveArtifacts "${vpkgDirPath}vehiclepkg_${packageName}_${appName}.tar"
     }
 
     def updateVechicleManifestFile(def filePath, def swPackageSize){
@@ -150,7 +174,7 @@ class OtaNgStages {
             // Write the modified content back to the file
             script.writeFile(file: filePath, text: fileContent)
 
-            logger.info("Updated AVehicle Manifest Json file.")
+            logger.info("Updated A Vehicle Manifest Json file.")
         }
         else{
             logger.error("file : $filePath not exists")
